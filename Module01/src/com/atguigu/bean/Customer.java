b7413fb5222ef163ceeaad1f118813c304aeaccb
““wjl <l>
2018-10-28 20:25:51 +0800

    Initial commit

---
+++ Module01/src/com/atguigu/bean/Customer.java
@@ -0,0 +1,43 @@
+package com.atguigu.bean;
+
+import com.sun.org.apache.xpath.internal.operations.String;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//这是单行注释
+public class Customer {
+
+
+    private static final Customer cst = new Customer();
+
+    public static void main(String[] args) {
+        ArrayList list = new ArrayList();
+        Customer customer = new Customer();
+    }
+    public void method(){
+        List list = new ArrayList();
+        for (Object o : list) {
+
+        }
+        for (int i = 0; i < list.size(); i++) {
+
+        }
+        for (int i = list.size() - 1; i >= 0; i--) {
+            
+        }
+        if (list == null) {
+
+        }
+        if (list != null) {
+
+        }
+        if (list != null) {
+
+        }
+        if (list == null) {
+
+        }
+    }
+
+}
